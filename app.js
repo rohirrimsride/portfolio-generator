ea264a6812317c9e14f1cb1332ca30020171a30b
Strider <dave.barnes23@gmail.com>
2022-06-17 17:59:11 -0500

    matching code snap shot

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-console.log(profileDataArgs);
-
-
-
-
-
-
-
-
+const profileDataArgs = process.argv.slice(2);
 
 const printProfileData = profileDataArr => {
+
     for (let i = 0; i < profileDataArr.length; i += 1) {
         console.log(profileDataArr[i]);
     }
@@ -18,7 +9,7 @@ const printProfileData = profileDataArr => {
 
     console.log('=============');
 
-    profileDataArr.forEach(profileThingy => console.log(profileThingy));
+    profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 
 printProfileData(profileDataArgs);
