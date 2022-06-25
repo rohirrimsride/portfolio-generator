3f54a583ad5c8e45f6c1d09835196e6f4a907314
Strider <dave.barnes23@gmail.com>
2022-06-25 12:49:56 -0500

    making sure everythings committed from this feature

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-// const fs = require('fs');
+const fs = require('fs');
 const inquirer = require('inquirer');
-// const generatePage = require('./src/page-template.js');
+const generatePage = require('./src/page-template.js');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -134,19 +134,13 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('index.html', pageHTML, err =>{
+            if (err) throw err;
+
+        // console.log('Portfolio complete! Check out index.html to see the output!');
+        // });
+        // console.log(portfolioData);
+        });
     });
-    
-
-
-
-
-// const pageHTML = generatePage(name, github);
-
-// const [name, github] = profileDataArgs;
-
-// fs.writeFile('index.html', pageHTML, err =>{
-//     if (err) throw err;
-
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-// });
